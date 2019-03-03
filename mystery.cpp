b5c6d40c3c6e55aa82004382ba9a9af598adae4d
doctordre246 <doctordre@users.noreply.github.com>
2019-03-03 17:39:51 -0400

    Added source file

--- mystery.cpp
+++ mystery.cpp
@@ -14,9 +14,9 @@ void mystery1(auto& Data)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+  for ( unsigned int i = 0 ; i < Data.size( ) ; i++)
   {
-    for ( int j = 0 ; j < i ; j++)
+    for ( unsigned int j = 0 ; j < i ; j++)
 	if ( Data[ i ] < Data[ j ] )
 	    swap( Data[ i ] , Data[ j ] );
 
@@ -25,7 +25,33 @@ void mystery1(auto& Data)
 
 }
 
-//... Other mysteries...
+void mystery2(auto& Data)
+{
+	// mystery 2
+	cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+for (unsigned int i = 0 ; i < Data . size ( ) ; i++)
+{
+	for (unsigned int j = 0 ; j < Data.size( ) -1 ; j++)
+		if ( Data [ j+1] < Data [ j ] )
+		swap ( Data [ j +1] , Data [ j ] ) ;
+		
+	print(Data);
+}
+}
+void mystery3(auto& Data)
+{
+	// mystery 3
+	cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+for (unsigned int i = 0 ; i < Data.size ( ) ; i++)
+{
+	for (unsigned int j = i ; j < Data.size ( ) ; j++)
+	if ( Data [ j ] < Data [ i ] )
+	swap ( Data [ i ] , Data [ j ] ) ;
+	
+	print(Data);
+}
+}
+
 
 int main()
 {
@@ -41,3 +67,4 @@ int main()
   mystery3(D3);
 
 }
+
